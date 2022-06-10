c4a684c4b080d5da69af8cd8b7610120f6c7b51b
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:36:04 +0700

    Add files via upload

---
+++ ulpr 3/ulpr 3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 3 задание
+namespace z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Год, который вы хотите ввести: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 12 + 1; i++)
+            {
+                DateTime date = new DateTime(n, i, DateTime.DaysInMonth(n, i));
+                Console.WriteLine($"В {date.Month} месяце: {date.Day - (7 + (int)date.DayOfWeek - 4) % 7} четверг");
+            }
+        }
+    }
+}
