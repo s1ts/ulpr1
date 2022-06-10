0fecf134cc924d8758c2b9fec7945e47638084dd
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:38:04 +0700

    Add files via upload

---
+++ ulpr 2/ulpr 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); //day
+            int b = int.Parse(Console.ReadLine()); //month 
+            int c = int.Parse(Console.ReadLine()); //year
+            var dateJ = new DateTime(c, b, a);
+            Console.WriteLine(dateJ.ToString());
+            //
+            int C = c / 100;
+            int C1 = C / 4;
+            double n = C - C1 - 2;
+            a += (int)n;
+            var dateG = new DateTime(c, b, a);
+            Console.WriteLine(dateG.ToString(System.Globalization.CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
