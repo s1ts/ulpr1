be289375e07f4a3f7aab0e2815a0d16e35c2b1e3
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:31:38 +0700

    Add files via upload

---
+++ ulprParnaya1/ulprParnaya1/Program.cs
@@ -0,0 +1,6 @@
+﻿//2 задание
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+
