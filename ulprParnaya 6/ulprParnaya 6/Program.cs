0ebd8fdd2bed51163a295578d17209c678dd054c
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:33:28 +0700

    Add files via upload

---
+++ ulprParnaya 6/ulprParnaya 6/Program.cs
@@ -0,0 +1,20 @@
+﻿//6 zadanie
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int i;
+int sum = 0;
+if ((a < 0 || b < 0) | a > b)
+{
+    Console.WriteLine("неверный промежуток");
+}
+else
+{
+    for (i = a; i <= b; i++)
+    {
+        if (i % 4 == 0)
+        {
+            sum += i;
+        }
+    }
+}
+Console.WriteLine(sum);
