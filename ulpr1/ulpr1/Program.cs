641c6b99314879d00ced8f178d87f8e0e83c224a
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:34:18 +0700

    Add files via upload

---
+++ ulpr1/ulpr1/Program.cs
@@ -0,0 +1,37 @@
+﻿ // 1 задача без вариантов
+using System;
+using System.Globalization;
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt;
+            int day = int.Parse(Console.ReadLine());
+            int mounth = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            string date = $"{year}, {mounth}, {day}";
+            int countDaysOfEndOfYear;
+            GregorianCalendar calendar = new GregorianCalendar();
+            if (DateTime.TryParse(date, out dt))
+            {
+                Console.WriteLine(calendar.GetDayOfYear(dt));
+                if (calendar.IsLeapYear(year))
+                {
+                    countDaysOfEndOfYear = 366 - calendar.GetDayOfYear(dt);
+
+                }
+                else
+                {
+                    countDaysOfEndOfYear = 365 - calendar.GetDayOfYear(dt);
+                }
+                Console.WriteLine(countDaysOfEndOfYear);
+            }
+            else
+            {
+                Console.WriteLine("Некорректная дата ");
+            }
+        }
+    }
+}
