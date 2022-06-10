b97fb330a95918a261e6a93b84f3a5b608c14958
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:38:45 +0700

    Add files via upload

---
+++ ulpr 4/ulpr 4/Program.cs
@@ -0,0 +1,33 @@
+﻿//4 задание
+Random ran = new Random();
+int y = 1;
+Console.WriteLine("Введите число, до которого загадывать:");
+int r = int.Parse(Console.ReadLine());
+int x = ran.Next(y, r);
+int z = ran.Next(1, 10);
+Console.WriteLine($"Количество попыток:{z}");
+for (int i = 1; i <= z; i++)
+{
+    Console.WriteLine($"Введите число от 1 до {r}:");
+    int n = int.Parse(Console.ReadLine());
+    if (i == z)
+    {
+        Console.WriteLine("Вы не угадали. Попыток больше нет.");
+    }
+    else
+    {
+        if (n < x)
+        {
+            Console.WriteLine($"Вы не угадали. Загаданное число больше. Осталось  { z - i} попыток");
+        }
+        if (n > x)
+        {
+            Console.WriteLine($"Не угадали. Загаданное число меньше. Осталось {z - i} попыток");
+        }
+        if (n == x)
+        {
+            Console.WriteLine("Вы угадали!");
+            return;
+        }
+    }
+}
