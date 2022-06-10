7e108995952d916f20acd5e48b0ad64680d6a0de
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:37:00 +0700

    Add files via upload

---
+++ ulpr 5/ulpr 5/Program.cs
@@ -0,0 +1,13 @@
+﻿// 5 задание
+class program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(".......");
+        Parallel.For(0, 100, x => Console.Write((x = new Random().Next(a, b)) != 0 ?
+       x.ToString() + "\n" : null));
+        Console.ReadKey();
+    }
+}
