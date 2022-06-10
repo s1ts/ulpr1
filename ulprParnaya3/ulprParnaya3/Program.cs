47b4cde7025c60c0fd9d097d2bcfe9986b89c1c0
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-10 16:32:06 +0700

    Add files via upload

---
+++ ulprParnaya3/ulprParnaya3/Program.cs
@@ -0,0 +1,16 @@
+﻿//3 задание
+var array = new int[] { 2, 3, 4, 6, 9, 3 };
+
+int first = array.First();
+int last = array.Last();
+
+int countDivFirst = array.Count(i => i % first == 0);
+int countDivLast = array.Count(i => i % last == 0);
+if (countDivFirst > countDivLast)
+{
+    Console.WriteLine(countDivFirst);
+}
+else
+{
+    Console.WriteLine(countDivLast);
+}
