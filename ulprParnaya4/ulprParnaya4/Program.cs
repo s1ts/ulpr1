8de25f32625992fdb71cf01faeb62cb191abc6ac
s1ts <107190958+s1ts@users.noreply.github.com>
2022-06-11 15:04:25 +0700

    Add files via upload

---
+++ ulprParnaya4/ulprParnaya4/Program.cs
@@ -0,0 +1,26 @@
+﻿//4 задане
+
+namespace z4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число для преобразования ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; ; i++)
+            {
+                if (num == 0) break;
+
+                if (i != 0) result *= 10;
+
+                result += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
